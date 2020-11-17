eee1ec5fa6a3b5ecf5aff5ba964e3857d8da9e8b
lydzb123 <lydiabaik@gmail.com>
2020-11-17 00:15:24 -0800

    Add modal to language form

--- public/components/LanguageForm.jsx
+++ public/components/LanguageForm.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import Modal from './Modal.jsx'
+
+const More = styled.div`
+  font-weight: bold;
+`;
+
 
 
 const LanguageForm = ({languages, totalReviews, handleFilterClick}) => {
   const languageOptions = ["All Languages", "English", "Chinese(Trad.)", "Japanese"]; //dynamically render this? get all languages?
 
+  const [status, setStatus] = useState(false);
 
 
   return (
@@ -16,13 +24,33 @@ const LanguageForm = ({languages, totalReviews, handleFilterClick}) => {
           <input type="radio" className="languageFilter" name="languages" value="allLanguages" onClick={handleFilterClick}/>All Languages ({totalReviews})
         </label>
       </div>
+
+      {languages.slice(0,3).map((option, i) => (
+        <div className ="inputLine" key={i}>
+            <label>
+              <input type="radio" className="languageFilter" name="languages" value={option.language} onClick={handleFilterClick}/>{option.language} ({option.total})
+            </label>
+        </div>
+      ))}
+
+    { status && (<Modal closeModal={() => setStatus(false)}>
+      <div className ="inputLine">
+          <label>
+            <input type="radio" className="languageFilter" name="languages" value="allLanguages" onClick={handleFilterClick}/>All Languages ({totalReviews})
+          </label>
+        </div>
         {languages.map((option, i) => (
           <div className ="inputLine" key={i}>
               <label>
                 <input type="radio" className="languageFilter" name="languages" value={option.language} onClick={handleFilterClick}/>{option.language} ({option.total})
               </label>
-          </div>
-        ))}
+          </div>))}
+      </Modal>)}
+
+        <div className="container">
+        <More onClick={() => setStatus(true)}>More</More>
+        </div>
+
 
     </div>
   )
---
+++ public/components/Modal.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Overlay = styled.div`
+  position: fixed;
+  display: block;
+  overflow: auto;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.5);
+  z-index: 999;
+  cursor: pointer;
+`;
+
+const Content = styled.div`
+  margin: 15% auto;
+  background-color: white;
+  width:204px;
+  height: 444px;
+  padding: 44px;
+  position: relative;
+  font-size: 16px;
+  line-height: 34px;
+`;
+
+
+
+const Modal = (props) => {
+
+
+const closeIcon = () => (
+  <div onClick={closeModal}
+  style={{
+    color: '#000000',
+    padding: '10px',
+    cursor: 'pointer',
+    backgroundColor: 'transparent',
+    border: 0,
+    position: 'absolute',
+    top: '0.3rem',
+    right: '0.5rem',
+  }}>✕</div>
+);
+
+
+  const {closeModal} = props;
+  return(
+    <Overlay>
+      <Content>
+        {closeIcon()}
+        {props.children}
+      </Content>
+    </Overlay>
+  )
+};
+
+
+
+export default Modal;
