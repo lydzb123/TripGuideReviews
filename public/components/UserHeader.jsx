4f704fe7adde5fbb1965f27ca64f263894d84b2c
lydzb123 <lydiabaik@gmail.com>
2020-11-18 01:25:14 -0800

    Add location icon to user header

--- public/components/UserHeader.jsx
+++ public/components/UserHeader.jsx
@@ -5,19 +5,25 @@ import styled from 'styled-components';
 const UserHeaderBar = styled.div`
 display: flex;
 
-img {
+.profilepic {
   border-radius: 50%;
   width: 42px;
   height: 42px;
   margin: 12px 12px 12px 24px;
+}
 
+.location {
+  width: 17px;
+  height: 17px;
+  margin-top: 3px;
+  vertical-align: sub;
+  display: inline-block;
 }
 
 .userHead {
   margin: 14px 0 0 0;
   display: block;
   font-size: 12px;
-
 }
 
 .userInfo {
@@ -27,13 +33,11 @@ img {
 `;
 
 const UserHeader = ({name, profilePhoto, reviewDate, location, contributions}) => {
-
-
   return(
     <UserHeaderBar>
-      <img src={profilePhoto}/>
+      <img className="profilepic" src={profilePhoto}/>
       <div className="userHead"><b>{name}</b> wrote a review {reviewDate}
-      <div className="userInfo">{location} • {contributions} contributions</div></div>
+      <div className="userInfo"><img className="location" src="https://tripreviews.s3-us-west-1.amazonaws.com/icons8-marker-50.png"></img>{location} • {contributions} contributions</div></div>
     </UserHeaderBar>
 
   )
